d4150618801614ef15bd85dd88e0fa04141d68c9
Garrett Campbell <campg3@vt.edu>
2020-11-23 12:47:35 -0500

    grabbing list of errors and displaying

--- src/main/java/cryptoguard/plugin/ViewAction.java
+++ src/main/java/cryptoguard/plugin/ViewAction.java
@@ -7,16 +7,22 @@ import com.intellij.openapi.fileChooser.FileChooserDescriptor;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.VirtualFile;
 import frontEnd.Interface.outputRouting.ExceptionHandler;
+import frontEnd.MessagingSystem.routing.structure.Default.Issue;
 import frontEnd.MessagingSystem.routing.structure.Default.Report;
 import org.apache.commons.io.FileUtils;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewAction extends AnAction {
 
+    private List<Issue> issuesList = new ArrayList<>();
+
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
+        issuesList.clear();
         FileChooserDescriptor fileChooser = new FileChooserDescriptor(
                 true,
                 true,
@@ -34,8 +40,18 @@ public class ViewAction extends AnAction {
             File file = new File(param.getPath());
             try {
                 Report r = Report.deserialize(new File(param.getPath()));
-                r.getIssues();
-                Messages.showMessageDialog(e.getProject(), Integer.toString(r.getIssues().size()), "Path", Messages.getInformationIcon());
+                issuesList = r.getIssues();
+                String str = "Project name: " + e.getProject().getName() +
+                        "\nNumber of crypto errors: " + Integer.toString(r.getIssues().size()) + "\n";
+                for (Issue i : r.getIssues()) {
+                    str = str + i.getMessage() + "\n";
+                }
+                Messages.showMessageDialog(
+                        e.getProject(),
+                        str,
+                        "Path",
+                        Messages.getInformationIcon()
+                );
             } catch (ExceptionHandler exceptionHandler) {
                 exceptionHandler.printStackTrace();
             }
